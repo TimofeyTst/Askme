d5c9719660a093ce9b1b5035f4242bb8a2fbf78a
TimofeyTst <yarik.mist@gmail.com>
2022-12-26 13:40:34 +0300

    Avatars added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :questions, dependent: :destroy
+  has_one_attached :avatar
+
+  validates :nickname, presence: true
+  validates :email, presence: true
 
   validates_uniqueness_of :email
   validates_uniqueness_of :nickname
